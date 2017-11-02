388772c1a1a14666a9c6643b88c67e3cb6234f45
Adrian Quintana <adrianquintana@gmail.com>
2017-11-02 09:17:35 +0000

    some polish and documentation

--- pygeppetto_blueprint.py
+++ pygeppetto_blueprint.py
@@ -3,13 +3,6 @@ from flask import Flask, send_from_directory, jsonify, render_template
 
 pygeppetto_core = Blueprint('pygeppetto_core', __name__, template_folder='org.geppetto.frontend/src/main/webapp/build')
 pygeppetto_core.debug = True
-# pygeppetto_core.config['ERROR_404_HELP'] = False
-
-
-# @pygeppetto_core.errorhandler(404)
-# def page_not_found(e):
-#     return jsonify(error=404, text="The requested URL was not found on the server"), 404
-
 
 @pygeppetto_core.route('/')
 def geppetto(name=None):
